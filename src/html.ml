b8d3c2a3acbf36ce426aa2277f8ca77878f97e01
Kate <kit.ty.kate@disroot.org>
2022-11-21 20:43:02 +0000

    Version 0.2.0~alpha1

--- src/html.ml
+++ src/html.ml
@@ -16,6 +16,6 @@ let index =
          p [txt "something something"];
        ];
        footer ~a:[a_style "padding-top: 5em;"] [
-         small [txt "Powered by MirageOS / exn.st v0.1.0"];
+         small [txt "Powered by MirageOS / exn.st v0.2.0~alpha1"];
        ];
      ])
